2a01dc85f55398feff15daf034649952e1348041
Alva.Arnaez <alva.arnaez.estrada@gmail.com>
2026-02-13 03:12:56 +0100

    edite los textos de bio y curious, ademas agregue email y phone

--- src/App.tsx
+++ src/App.tsx
@@ -44,11 +44,16 @@ function App() {
     </div>
     <h2 className='bio-header'>BIO</h2>
     <p>
-        Alva is a video creator and content producer based in Berlin. She grew up in Lima studying music and composing songs nobody heard. Later came advertising, then business, picking up cameras somewhere in between and figuring out the rest as she went. She founded Montania Films and spent five years directing music videos, theater trailers and brand content. Before that, she produced work for brands like PlayStation, Jeep, and Apple at Vatio Productions. At Berlin indie studio Maschinen Mensch, she made trailers and social content for Mother Machine.
+        I'm Alva — a trans woman, video creator and content producer based in Berlin. I grew up in Lima studying music and composing songs nobody heard. Later came advertising, then business, picking up cameras somewhere in between and figuring out the rest as I went. I founded Montania Films and spent five years directing music videos, theater trailers and brand content. Before that, I produced work for brands like PlayStation, Jeep, and Apple at Vatio Productions. At Berlin indie studio Maschinen Mensch, I made trailers and social content for Mother Machine.
       </p>
       <h2>CURIOUS FACTS</h2>
-        <p>She streams on Twitch as LelouLavender. Cozy shooters, indie games, and the kind of conversations you'd have at 2am with someone you just met. Video games have been part of her life since childhood, growing up with Nintendo and PlayStation. Her favorite is Final Fantasy IX, but she also has a special connection to Crash Team Racing, a game that, according to her, once genuinely saved her life when years of virtual drifting kicked in on a wet highway. She's such a gamer that she even figured out how to edit videos using a PS4 controller.
+        <p>I stream on Twitch as LelouLavender were we Co-work, play cozy shooters, indie games or talk about cute and vulnerable stuff. Video games have been part of my life since childhood, growing up with Nintendo and PlayStation. My favorite is Final Fantasy IX, but I also have a special connection to Crash Team Racing, a game that once saved my life when years of virtual drifting kicked in on a wet highway. I'm such a gamer that I even figured out how to edit videos using a PS4 controller.
       </p>
+    <h2>CONTACT</h2>
+    <div className="contact">
+      <a href="mailto:alva.arnaez.estrada@gmail.com">alva.arnaez.estrada@gmail.com</a>
+      <a href="tel:+4915563132433">+49 155 6313 2433</a>
+    </div>
     </div>
   );
 }
