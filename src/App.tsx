feb1d901ce58599ff117f6547bd7126afa6fef94
Alva.Arnaez <alva.arnaez.estrada@gmail.com>
2026-02-18 11:07:19 +0100

    video producer and content creator exchanged

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     </div>
     <h2 className='bio-header'>BIO</h2>
     <p>
-        I'm Alva — a trans woman, video creator and content producer based in Berlin. I grew up in Lima studying music and composing songs nobody heard. Later came advertising, then business, picking up cameras somewhere in between and figuring out the rest as I went. I founded Montania Films and spent five years directing music videos, theater trailers and brand content. Before that, I produced work for brands like PlayStation, Jeep, and Apple at Vatio Productions. At Berlin indie studio Maschinen Mensch, I made trailers and social content for Mother Machine.
+        I'm Alva — a trans woman, Content Creator and Video Producer based in Berlin. I grew up in Lima studying music and composing songs nobody heard. Later came advertising, then business, picking up cameras somewhere in between and figuring out the rest as I went. I founded Montania Films and spent five years directing music videos, theater trailers and brand content. Before that, I produced work for brands like PlayStation, Jeep, and Apple at Vatio Productions. At Berlin indie studio Maschinen Mensch, I made trailers and social content for Mother Machine.
       </p>
       <h2>CURIOUS FACTS</h2>
         <p>I stream on Twitch as LelouLavender were we Co-work, play cozy shooters, indie games or talk about cute and vulnerable stuff. Video games have been part of my life since childhood, growing up with Nintendo and PlayStation. My favorite is Final Fantasy IX, but I also have a special connection to Crash Team Racing, a game that once saved my life when years of virtual drifting kicked in on a wet highway. I'm such a gamer that I even figured out how to edit videos using a PS4 controller.
